38fa4742be70a19635dfb5d218831344f43123bf
Satya Ganesh <satyaganesh399@gmail.com>
2025-05-25 18:18:13 +0530

    name change

--- src/Screens/PlayGround/Navbar.tsx
+++ src/Screens/PlayGround/Navbar.tsx
@@ -1,65 +1,66 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components'
-import { DarkModeContext } from '../../DarkModeContext/DarkModeContext';
-import DarkModeToggleButton from '../HomeScreen/DarkModeToggleButton';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { DarkModeContext } from "../../DarkModeContext/DarkModeContext";
+import DarkModeToggleButton from "../HomeScreen/DarkModeToggleButton";
 
 const NavbarContainer = styled.div`
-width : '7vw';
-height : 4.5rem;
-background : #241f21;
-display : flex;
-align-items : center;
-justify-content : center;
+  width: "7vw";
+  height: 4.5rem;
+  background: #241f21;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const NavbarContent =styled.button`
-display : flex;
-align-items: center;
-gap : 0.9rem;
-border : 0;
-outline : 0;
-background : transparent;
-`
+const NavbarContent = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.9rem;
+  border: 0;
+  outline: 0;
+  background: transparent;
+`;
 
 const Logo = styled.img`
-width : 45px;
-height : 45px;
-`
+  width: 45px;
+  height: 45px;
+`;
 const MainHeading = styled.h1`
-font-size : 1.9rem;
-font-weight : 400;
-color : white;
+  font-size: 1.9rem;
+  font-weight: 400;
+  color: white;
 
-span{
-  font-weight : 700;
-}
-`
+  span {
+    font-weight: 700;
+  }
+`;
 
 function Navbar() {
-
   const navigate = useNavigate();
   // DarkMode Switch
   const darkTheme = React.useContext(DarkModeContext)!;
   let isDarkThemeOn = darkTheme.isDarkModeOn;
   let SetIsDarkThemeOn = darkTheme.setIsDarkModeOn;
 
-  function changeTheme(){
+  function changeTheme() {
     SetIsDarkThemeOn(!isDarkThemeOn);
   }
   return (
     <NavbarContainer>
-      <NavbarContent onClick={() => {
-        navigate('/')
-      }}>
+      <NavbarContent
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         <Logo src="/logo.png" alt="" />
         <MainHeading>
-          <span>Code</span>Deck
+          <span>Codex</span>
         </MainHeading>
       </NavbarContent>
-      <DarkModeToggleButton changeTheme ={changeTheme} />
+      <DarkModeToggleButton changeTheme={changeTheme} />
     </NavbarContainer>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
